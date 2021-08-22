61d7c49d5447038d5a84027928864dd11df73e51
ETC <etolpygo@gmail.com>
2021-08-22 01:17:31 +0100

    add heroes service and fetch mock heroes synchronously

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +9,17 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
-
-  heroes = HEROES;
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
