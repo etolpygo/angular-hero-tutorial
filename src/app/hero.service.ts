70dcd8878f5482aca1e75989c7748a39b10efe10
ETC <etolpygo@gmail.com>
2021-08-22 01:22:00 +0100

    simulate getting data from the server asynchronously

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -7,8 +9,9 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
   constructor() { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
   
   ngOnInit() {
